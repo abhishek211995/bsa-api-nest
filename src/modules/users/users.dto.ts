ee323705df975676495bc59a7380a66d1ff30613
Abhimanyu Patil <chanondler@Abhimanyus-MacBook-Air.local>
2023-04-27 19:45:41 +0530

    individual reg fixes

--- src/modules/users/users.dto.ts
+++ src/modules/users/users.dto.ts
@@ -49,16 +49,25 @@ export class CreateUserDto extends BreederDto {
 export class IndividualUserDto {
   @IsString()
   user_name: string;
+
   @IsString()
   email: string;
+
   @IsString()
   password: string;
+
   @IsString()
   contact_no: string;
+
   // @IsString()
   identity_doc_name: string;
+
   @IsString()
-  identification_no: string;
+  identification_id_no: string;
+
+  @IsString()
+  identification_id_name: string;
+
   @IsString()
   user_country: string;
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -120,6 +120,7 @@ export class UsersService {
 
       return { user };
     } catch (error) {
+      console.log("error here", error);
       throw error;
     }
   }
