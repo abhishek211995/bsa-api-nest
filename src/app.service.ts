3f6db19fee93efbd51fb0c038e34562a6174aa94
Abhimanyu Patil <chanondler@Abhimanyus-MacBook-Air.local>
2023-05-29 12:08:07 +0530

    version

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@nestjs/common";
+import * as pkg from "../package.json";
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Hello World!";
+    return `Hello World! v${pkg.version}`;
   }
 }
