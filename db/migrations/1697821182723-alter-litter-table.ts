9044d32a64fe98ac5ffc9ee0e867b022c8179e9d
Abhimanyu Patil <abhimanyu.patil39@gmail.com>
2023-11-06 17:36:34 +0000

    fix migration scripts

--- db/migrations/1697821182723-alter-litter-table.ts
+++ db/migrations/1697821182723-alter-litter-table.ts
@@ -3,29 +3,6 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class AlterLitterTable1697821182723 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        ALTER TABLE bre_litter_registration
-          DROP FOREIGN KEY FK_3a4443a160c780e65ae83cf0c2e,
-          DROP FOREIGN KEY FK_85e906894fb774e202c4b806aaf;
-      `);
-
-    await queryRunner.query(`
-        ALTER TABLE bre_litter_registration
-          CHANGE COLUMN sire_id sire_id VARCHAR(255) NULL,
-          CHANGE COLUMN sire_owner_id sire_owner_id INT NULL;
-          CHANGE COLUMN remarks remarks json null;
-      `);
-
-    await queryRunner.query(`
-      ALTER TABLE bre_litter_registration 
-        ADD CONSTRAINT FK_3a4443a160c780e65ae83cf0c2e
-          FOREIGN KEY (sire_owner_id)
-          REFERENCES bre_user (id),
-        ADD CONSTRAINT FK_85e906894fb774e202c4b806aaf
-          FOREIGN KEY (sire_id)
-          REFERENCES bre_animal (animal_id);
-      `);
-
-    await queryRunner.query(`
         ALTER TABLE bre_litter_registration 
           ADD COLUMN semen_bill VARCHAR(256),
           ADD COLUMN vet_certificate VARCHAR(256),
